886bce2bda0c28b9e45683d9b2291567d702b1ff
H0AX3N <sandipandebgithub@gmail.com>
2025-11-15 00:08:06 +0530

    used use hook

--- app/articles/[articleId]/page.tsx
+++ app/articles/[articleId]/page.tsx
@@ -1,18 +1,23 @@
+'use client';
 import Link from 'next/link';
+import { use } from 'react';
 
-export default async function NewsArticle({
+export default function NewsArticle({
     params, searchParams
 } : {
     params: Promise<{ articleId: string }>;
-    
+    searchParams: Promise<{ lang?: "en" | "es" }>;
 }) {
+    const { articleId } = use(params); // client components cannot use async await functions. so for that we have use hook that is provided by react
+    const { lang = "en" } = use(searchParams);
+
     return (
         <>
-            <h1>News article id</h1>
-            <p>Reading in language</p>
-            <div>
-                <Link href='articles/id?lang=en' className='underline'>English</Link>
-                <Link href='articles/id?lang=sn'>Spanish</Link>
+            <h1>News article {articleId}</h1>
+            <p>Reading in {lang}</p>
+            <div className='flex gap-4 mt-4'>
+                <Link href={`${articleId}?lang=en`} className='underline'>English</Link>
+                <Link href={`${articleId}?lang=es`} className='underline'>Spanish</Link>
             </div>
         </>
     )
